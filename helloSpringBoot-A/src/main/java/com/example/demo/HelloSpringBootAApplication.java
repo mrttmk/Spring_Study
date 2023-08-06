058c2e1e9443941781b82890b714d0571c3a5c83
森田智稀 <t.morita018@fmeets.com>
2023-08-06 12:06:43 +0900

    1-3 viewとコントローラ 課題提出

---
+++ helloSpringBoot-A/src/main/java/com/example/demo/HelloSpringBootAApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootAApplication.class, args);
+	}
+
+}
